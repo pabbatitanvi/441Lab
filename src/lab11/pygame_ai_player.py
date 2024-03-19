90eb1e4f1386028fce77b37855ac58a2b96e29b9
Tanvi Pabbati <tmp5876@psu.edu>
2024-03-19 19:47:53 -0400

    lab 11

--- src/lab11/pygame_ai_player.py
+++ src/lab11/pygame_ai_player.py
@@ -1,12 +1,19 @@
-""" Create PyGameAIPlayer class here"""
+import random
+from turn_combat import CombatPlayer
 
 
 class PyGameAIPlayer:
-    pass
+    def selectAction(self, state):
+        return ord(str(random.randint(0,9)))
 
 
 """ Create PyGameAICombatPlayer class here"""
 
 
-class PyGameAICombatPlayer:
-    pass
+class PyGameAICombatPlayer(CombatPlayer):
+    def __init__(self, name):
+        super().__init__(name)
+
+    def weaponSelectingStrategy(self):
+        self.weapon = random.randint(0, 2)
+        return self.weapon
