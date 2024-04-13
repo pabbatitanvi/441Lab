800a50271568131d70c5dfb78304417d2e35b616
Tanvi Pabbati <tmp5876@psu.edu>
2024-04-13 12:09:15 -0400

    pygame combat change

--- src/lab11/pygame_combat.py
+++ src/lab11/pygame_combat.py
@@ -11,11 +11,9 @@ AI_SPRITE_PATH = Path("assets/ai.png")
 pygame.font.init()
 game_font = pygame.font.SysFont("Comic Sans MS", 15)
 
-
 class PyGameComputerCombatPlayer(CombatPlayer):
     def __init__(self, name):
         super().__init__(name)
-
     def weapon_selecting_strategy(self):
         if 30 < self.health <= 50:
             self.weapon = 2
@@ -24,8 +22,6 @@ class PyGameComputerCombatPlayer(CombatPlayer):
         else:
             self.weapon = 0
         return self.weapon
-
-
 def draw_combat_on_window(combat_surface, screen, player_sprite, opponent_sprite):
     screen.blit(combat_surface, (0, 0))
     player_sprite.draw_sprite(screen)
@@ -34,9 +30,9 @@ def draw_combat_on_window(combat_surface, screen, player_sprite, opponent_sprite
     screen.blit(text_surface, (50, 50))
     pygame.display.update()
 
-
 def run_turn(currentGame, player, opponent):
     players = [player, opponent]
+    states = list(reversed([(player.health, player.weapon) for player in players]))
     states = list([tuple(player.health for player in players)]*2)
     for current_player, state in zip(players, states):
         current_player.selectAction(state)
@@ -46,21 +42,16 @@ def run_turn(currentGame, player, opponent):
     print("%s's health = %d" % (player.name, player.health))
     print("%s's health = %d" % (opponent.name, opponent.health))
     reward = currentGame.checkWin(player, opponent)
-    return reward
-
 def run_pygame_combat(combat_surface, screen, player_sprite):
     currentGame = Combat()
     player = PyGameHumanCombatPlayer("Legolas")
     """ Add a line below that will reset the player object
     to an instance of the PyGameAICombatPlayer class"""
-
     opponent = PyGameComputerCombatPlayer("Computer")
     opponent_sprite = Sprite(
         AI_SPRITE_PATH, (player_sprite.sprite_pos[0] - 100, player_sprite.sprite_pos[1])
     )
-
     # Main Game Loop
     while not currentGame.gameOver:
         draw_combat_on_window(combat_surface, screen, player_sprite, opponent_sprite)
-
         run_turn(currentGame, player, opponent)
